d402feedc4d583f65283bcee020f23eef805343e
Matt Espe <mespe@ucdavis.edu>
2019-04-05 07:47:19 -0600

    Updates.

--- tests/test_expand_bounds.R
+++ tests/test_expand_bounds.R
@@ -1,4 +1,4 @@
-bounds = list(dos = c(5, 2500), a = c(-0.2, 0), k = c(0, 0.2))
+bounds = list(dos = c(5, 2500), a = c(-0.2, -1e-5), k = c(1e5, 0.2))
 ans = OzoneExposure:::expand_bounds(bounds, 5)
 
 stopifnot(nrow(ans) == 5^3)
